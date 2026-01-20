8846d0d2953d76a0c8ae30863c5f991c4ce7c16b
Dylan M <dylanm12340@gmail.com>
2026-01-20 13:24:03 -0330

    CPU

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -6,45 +6,50 @@ import torch.nn as nn
 import time
 import torch.optim as optim
 
+def lab2(device):
+    print("using device:", device)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("using device:", device)
-
-if device.type == "cuda":
-    print(torch.cuda.get_device_name(0))
-
-
-transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-
-dataset = datasets.FakeData(
-    size=8000, image_size=(3, 224, 224), num_classes=10, transform=transform
-)
-
-loader = DataLoader(dataset, batch_size=32, shuffle=True)
+    if device.type == "cuda":
+        print(torch.cuda.get_device_name(0))
 
 
-# Train Model
-model = resnet50(weights=None)
-model.fc = nn.Linear(model.fc.in_features, 10)
-model = model.to(device)
+    transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
 
-# Timing
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    dataset = datasets.FakeData(
+        size=8000, image_size=(3, 224, 224), num_classes=10, transform=transform
+    )
 
-epochs = 2
-start = time.time()
+    loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-for epoch in range(epochs):
-    for images, labels in loader:
-        images = images.to(device)
-        labels = labels.to(device)
 
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    # Train Model
+    model = resnet50(weights=None)
+    model.fc = nn.Linear(model.fc.in_features, 10)
+    model = model.to(device)
 
-end = time.time()
-print("Training time (seconds): ", end - start)
+    # Timing
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+    epochs = 2
+    start = time.time()
+
+    for epoch in range(epochs):
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+    end = time.time()
+    print(f"{device.type} Training time (seconds): ", end - start)
+
+if __name__ == '__main__':
+    device = torch.device("cuda")
+    lab2(device)
+    device = torch.device("cpu")
+    lab2(device)
